39d68d347491822d10c5e5da0b4a5e0c5564ba78
Carlos Rojas <carlos.sadhu@gmail.com>
2018-06-06 08:43:29 +1000

    Generate ids for Customers

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,11 +2,15 @@
 
 class Customer
   attr_accessor :first_name, :last_name, :email, :phone
+  attr_reader :id
+  @@counter = 0
 
   def initialize(first_name, last_name, email, phone)
     @first_name = first_name
     @last_name = last_name
     @email = email
     @phone = phone
+    @@counter = @@counter + 1
+    @id = @@counter
   end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,7 +5,7 @@ require 'customer'
 describe Customer do
   subject(:customer) { described_class.new('Carlos', 'Rojas', 'carlos.sadhu@gmail.com', '0403931842') }
 
-  describe 'first_name' do
+  describe 'first_name accesors' do
     subject { customer.first_name }
 
     specify do
@@ -14,7 +14,7 @@ describe Customer do
     end
   end
 
-  describe 'last_name' do
+  describe 'last_name accesors' do
     subject { customer.last_name }
 
     specify do
@@ -23,7 +23,7 @@ describe Customer do
     end
   end
 
-  describe 'email' do
+  describe 'email accesors' do
     subject { customer.email }
 
     specify do
@@ -32,12 +32,23 @@ describe Customer do
     end
   end
 
-  describe 'phone' do
+  describe 'phone accesors' do
     subject { customer.phone }
 
     specify do
       customer.phone = '66666666666'
       expect(customer.phone).to eq '66666666666'
+    end
+  end
+
+  describe 'generates ids sequentially' do
+    let(:first_customer) { described_class.new('Peter', 'Parker', 'spidey@bellroy.com', '123456789') }
+    let(:second_customer) { described_class.new('Peter', 'Parker II', 'spidey2@bellroy.com', '9876543219') }
+
+    specify do
+      first_customer
+      second_customer
+      expect(second_customer.id).to eq(first_customer.id + 1)
     end
   end
 end
